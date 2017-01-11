310b025da1862d3b510c5940ae5b29ffa9c2967c
DoctorGoat <thearcy@gmail.com>
2017-01-10 21:18:36 -0500

    script
    this is a messy script dumper with sample output. a lot needs to be done to it to make it not bad

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream elminage = File.Open("ULJM06091.bin", FileMode.Open);
+            StreamReader elmin = new StreamReader(elminage, Encoding.GetEncoding(932));
+            string TheEntireDamnGame = elmin.ReadToEnd();
+            Console.WriteLine(TheEntireDamnGame.Length);
+//            Regex r = new Regex("/守られている/g");
+            MatchCollection results = Regex.Matches(TheEntireDamnGame, "[ぁ-黑 ]{3,1000}");
+            Console.WriteLine(results.Count);
+            FileStream outputFile = new FileStream("output.txt", FileMode.OpenOrCreate);
+            StreamWriter write = new StreamWriter(outputFile, Encoding.GetEncoding(932));
+            StringBuilder outputBuilder = new StringBuilder();
+            foreach (Match m in results)
+            {
+                outputBuilder.Clear();
+                outputBuilder.Append(m.Index + "," + m.Value);
+                write.WriteLine(outputBuilder);
+             }
+            write.Close();
+            Console.ReadLine();
+        }
+    }
+}
